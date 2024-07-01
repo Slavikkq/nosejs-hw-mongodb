83c9acba341dfbcb4914eb244b106ba08c143be0
Slavikkq <yaroslavmanuljak@gmail.com>
2024-07-02 02:26:06 +0300

    hw 2

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -3,11 +3,7 @@ import { env } from '../utils/env.js';
 import { ENV_VARS } from '../constants/index.js';
 
 export const initMongoConnection = async () => {
-  const connectionLink = `mongodb+srv://${env(ENV_VARS.MONGODB_USER)}:${env(
-    ENV_VARS.MONGODB_PASSWORD,
-  )}@${env(ENV_VARS.MONGODB_URL)}/${env(
-    ENV_VARS.MONGODB_DB,
-  )}?retryWrites=true&w=majority&appName=M0DanilBoboshko`;
+  const connectionLink = `mongodb+srv://yaroslavmanuljak:<password>@cluster0.rnu1gqa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";`
 
   try {
     await mongoose.connect(connectionLink);
