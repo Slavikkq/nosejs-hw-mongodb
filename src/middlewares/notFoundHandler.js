add677ee581a5403a4f593bf38c7c4d8da880447
Slavikkq <yaroslavmanuljak@gmail.com>
2024-07-05 10:43:02 +0300

    hw3-crud

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,7 +1,4 @@
 export const notFoundHandler = (req, res, next) => {
-    if (next === 'fake') {
-    next();
-  }
   res.status(404).json({
     message: 'Route not found',
   });
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,47 +1,21 @@
-import { ContactsCollection } from '../db/contact.js';
+import { ContactsCollection } from '../db/models/contact.js';
 
-export const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
-};
+export const getAllContacts = async () => await ContactsCollection.find();
 
-export const getContactById = async (contactId) => {
-  const contact = await ContactsCollection.findById(contactId);
-  return contact;
-};
+export const getContactById = async (contactId) =>
+  await ContactsCollection.findById(contactId);
 
-export const createContact = async (payload) => {
-  //payload має бути таким
-  //{
-  // name - обов’язково
-  // phoneNumber - обов’язково
-  // email - не обовʼязково
-  // isFavourite - не обовʼязково
-  // contactType - не обовʼязково
-  //}
+export const createContact = async (payload) =>
+  await ContactsCollection.create(payload);
 
-  const contact = await ContactsCollection.create(payload);
-  return contact;
-};
-
-export const updateContact = async (contactId, payload, options = {}) => {
- 
-  const rawResult = await ContactsCollection.findOneAndUpdate(
-    { _id: contactId },
+export const upsertContact = async (contactId, payload) => {
+  const updatedContact = await ContactsCollection.findByIdAndUpdate(
+    contactId,
     payload,
-    {
-      new: true,
-      includeResultMetadata: true,
-      ...options,
-    },
+    { new: true },
   );
 
-  if (!rawResult || !rawResult.value) return null;
-
-  return {
-    contact: rawResult.value,
-    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
-  };
+  return updatedContact;
 };
 
 export const deleteContact = async (contactId) => {
